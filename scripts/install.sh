6b98cc8f116d133ae8647476b2700b095e48a891
pbillerot <philippe.billerot@gmail.com>
2019-08-29 11:00:04 +0200

    install service ko

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+### Abandon du script
+
 # Installation de drumservice sur Ubuntu
-# en mode sudo
+# Rester à la racine du projet
+# en mode sudo ./scripts/install.sh
 
 # Variables
 app=drumservice
@@ -9,17 +12,32 @@ final_path=/var/www/$app
 user_name=billerot
 
 # Dépendances système
+echo --- Installation des dépendances système
+echo --- Répertoire courant : 
+pwd
+
+read -rsp $"--- Dépendances système..." -n1 key
 apt install virtualenv python3-dev python3-virtualenv python3-pip
 
 # Copie de l'application
+read -rsp $"--- Copie de l'application..." -n1 key
 if [ -d "$final_path" ]; then
+  echo --- Suppression de $final_path
   rm -r "$final_path"
 fi
+ls -l /var/www
+
+read -rsp $"--- Copie dans $final_path..." -n1 key
 mkdir -p $final_path
 cp -r * $final_path/
+ls -l $final_path
+
 pushd $final_path
+echo --- Répertoire courant :
+pwd
 
 # Virtualenv
+read -rsp $"--- Installation de venv..." -n1 key
 virtualenv -p python3.6 venv
 set +o nounset 
 source venv/bin/activate
@@ -29,27 +47,36 @@ pip install gunicorn
 pip install pygame
 
 # gunicorn
+read -rsp $"--- Installation de gunicorn..." -n1 key
 sed -i -e "s/__APP_NAME__/$app/g" -e "s/__USER_NAME__/$user_name/g" gunicorn.py
 
 # Set permissions
+read -rsp $"--- Permissions sur $final_path..." -n1 key
 chown -R $user_name:www-data $final_path
+ls -l $final_path
 
 # Log folder
+read -rsp $"--- Log..." -n1 key
 if [ -d "/var/log/$app" ]; then
+  echo --- Suppression de /var/log/$app
   rm -r "/var/log/$app"
 fi
 mkdir -p /var/log/$app
-chown -R $app:www-data /var/log/$app
+chown -R $user_name:www-data /var/log/$app
+ls -l /var/log
 
 # Systemd config
-sed -i -e "s/__APP_NAME__/$app/g" -e "s/__USER_NAME__/$user_name/g" ../scripts/app.service
-cp ../scripts/app.service >/etc/systemd/system/$app.service
+read -rsp $"--- Systemd config..."  -n1 key
+sed -i -e "s/__APP_NAME__/$app/g" -e "s/__USER_NAME__/$user_name/g" ./scripts/app.service
+cp ./scripts/app.service /etc/systemd/system/$app.service
 
+read -rsp $"--- Systemctl reload..." -n1 key
 systemctl daemon-reload
 systemctl enable $app
-service add $app -l /var/log/$app/access.log
+service add $app.service -l /var/log/$app/access.log
 
 # démarrage
+read -rsp $"--- Systemctl reload..." -n1 key
 systemctl start $app
 
 popd
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source venv/bin/activate
+gunicorn --bind 127.0.0.1:8000 wsgi:app
